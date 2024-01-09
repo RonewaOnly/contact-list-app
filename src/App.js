14d131b730402695b6a719a61ac3564fc04ed81a
RonewaOnly <125514494+RonewaOnly@users.noreply.github.com>
2024-01-09 12:24:00 +0200

    contact-list react with API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState,useEffect } from 'react'
+import { ContactCard } from './ContactCard';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  //const contacts =[
+  //  {
+  //    avatarUrl: 'https://via.placeholder.com/150',
+  //    name:'Jenny Sum',
+  //    email:'jenny@gmail.com',
+  //    age: 20
+  //  },
+  //  {
+  //    avatarUrl: 'https://via.placeholder.com/150',
+  //    name:'Johnny Test',
+  //    email:'johnny.Test@sisterslab.com',
+  //    age: 102
+  //  },
+  //  {
+  //    avatarUrl: 'https://via.placeholder.com/150',
+  //    name:'Peter Pan',
+  //    email:'peter.pan@neverland.com',
+  //    age: 100
+  //  },
+  //];
+  const [ results , setResults] = useState([])
+  useEffect(() =>{
+    fetch("https://randomuser.me/api/?results=5")
+      .then(response => response.json())
+      .then(data => setResults(data.results))
+  },[]);
+
+  return(
+    <div>
+      {results.map((result,index)=>{
+        return(
+
+          <ContactCard//reading using arrays
+          avatarUrl = {result.picture.large                             }
+          name      = {result.name.first + ' '+ result.name.last        }
+          email     = {result.email                                     }
+          age       = {result.dob.age                                   }
+        />
+        );
+      })}
+    
+  
     </div>
   );
 }
